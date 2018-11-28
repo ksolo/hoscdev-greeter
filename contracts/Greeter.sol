f06eb47b3dcbb1cd8f3fabf28d321a1d8295aea4
Kevin Solorio <soloriok@gmail.com>
2018-11-27 21:00:22 -0800

    saying hello test passing

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -2,4 +2,8 @@ pragma solidity >= 0.4.25 < 0.6.0;
 
 contract Greeter {
 
+    function greet() external pure returns(string memory) {
+        return "Hello, World!";
+    }
+
 }
--- test/greeter_test.js
+++ test/greeter_test.js
@@ -5,4 +5,14 @@ contract("Greeter", () => {
     const greeter = await GreeterContract.deployed();
     assert(greeter, "contract has been deployed");
   });
+
+  describe("greet()", () => {
+    it("returns 'Hello, World!'", async () => {
+      const greeter = await GreeterContract.deployed();
+      const expected = "Hello, World!";
+      const actual = await greeter.greet();
+
+      assert.equal(actual, expected, "greeted with, 'Hello, World!'");
+    });
+  });
 });
